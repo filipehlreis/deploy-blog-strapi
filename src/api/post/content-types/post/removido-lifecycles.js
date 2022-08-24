e13bbfabaa3875a3814a8177a13cd12fac138340
Filipe Reis <filipe.henrique.reis@gmail.com>
2022-08-24 13:47:14 -0300

    Initial

---
+++ src/api/post/content-types/post/removido-lifecycles.js
@@ -0,0 +1,13 @@
+// 'use strict';
+
+// const axios = require('axios');
+
+// module.exports = {
+//   async afterCreate(result, data) {
+//     axios.post('https://api.netlify.com/build_hooks/6302e342eb51c70455d0604f');
+//   },
+
+//   async afterUpdate(result, params, data) {
+//     axios.post('https://api.netlify.com/build_hooks/6302e342eb51c70455d0604f');
+//   },
+// }
